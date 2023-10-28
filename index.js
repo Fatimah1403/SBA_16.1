2c732d0773997928e6e5e9a0dc1539ea60fd9800
Fatimah1403 <66882615+Fatimah1403@users.noreply.github.com>
2023-10-27 22:02:43 -0400

    Image folder added

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const pass_error = "Please fill up your Password";
 
 const validateEmail = () => {
     let emailVal = email.value;
-    let emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-z0-9]+\.[a-A-Z]{2,4}$/;
+    let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     let msg = []
 
    if (emailVal === "" || emailVal === null) {
